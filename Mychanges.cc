1a9c9d088796ea0112034823298f0c2a22797971
Yunlong Li <yunlong.li@cern.ch>
2021-07-01 12:33:09 +0000

    Update Mychanges.cc

--- Mychanges.cc
+++ Mychanges.cc
@@ -81,7 +81,7 @@ bool GOpticalSurface::isSpecular() const
     return false ;
 }
 
-//ggeo/X4Solid.cc
+//extg4/X4Solid.cc
 const bool X4Solid::convertPolycone_duplicate_py_inner_omission = true ;
 
 void X4Solid::convertPolycone()
@@ -94,3 +94,63 @@ void X4Solid::convertPolycone()
     unsigned nz = ph->Num_z_planes ;
     ...
 }
+
+//ok/OKMgr.cc
+OKMgr::OKMgr(int argc, char** argv, const char* argforced ) 
+    : 
+    m_log(new SLog("OKMgr::OKMgr","", debug)), 
+    m_ok(Opticks::HasInstance() ? Opticks::GetInstance() : new Opticks(argc, argv, argforced)), 
+    m_hub(new OpticksHub(m_ok)),            // immediate configure and loadGeometry  
+    m_idx(new OpticksIdx(m_hub)), 
+    m_num_event(m_ok->getMultiEvent()),     // after hub instanciation, as that configures Opticks 
+    m_gen(m_hub->getGen()), 
+    m_run(m_ok->getRun()), 
+    //m_viz(m_ok->isCompute() ? NULL : new OpticksViz(m_hub, m_idx, true)), 
+    m_viz(new OpticksViz(m_hub, m_idx, true)), //always do the visualization
+    m_propagator(new OKPropagator(m_hub, m_idx, m_viz)), 
+    m_count(0) 
+{ 
+    init(); 
+    (*m_log)("DONE"); 
+} 
+
+//optixrap/OContext.cc
+void OContext::CheckDevices(Opticks* ok)
+{
+    VisibleDevices vdev ;
+    LOG(info) << std::endl << vdev.desc();
+
+    BMeta* parameters = ok->getParameters();
+    parameters->add<int>("NumDevices", vdev.num_devices );
+    parameters->add<std::string>("VisibleDevices", vdev.brief() );
+
+    const char* frame_renderer = Opticks::Instance()->getFrameRenderer();
+    if( frame_renderer != NULL)
+    {
+        if(vdev.num_devices != 1) LOG(fatal) << "vdev.num_devices " << vdev.num_devices ;
+        assert( vdev.num_devices == 1 && "expecting only a single visible device, the one driving the display, in interop mode") ;
+        const char* optix_device = vdev.devices[0].name ;
+        LOG(LEVEL) << " frame_renderer " << frame_renderer ;
+        LOG(LEVEL) << " optix_device " << optix_device  ;
+        bool interop_device_match = SStr::Contains( frame_renderer, optix_device )  ;
+        //assert( interop_device_match && "OpenGL and OptiX must be talking to the same single device in interop mode"  ); 
+
+        parameters->add<std::string>("FrameRenderer", frame_renderer ); 
+    }
+    else 
+    { 
+        LOG(LEVEL) << " NULL frame_renderer : compute mode ? " ; 
+    } 
+}
+
+//optickscore/OpticksMode.cc 
+int OpticksMode::getInteractivityLevel() const 
+{ 
+    int interactivity = SSys::GetInteractivityLevel() ; 
+    if(m_noviz) interactivity = 0 ; 
+    if(isCompute()) interactivity = 0 ;  
+    //return interactivity  ; 
+    return 1  ; // always do the visualization 
+}
+
+
